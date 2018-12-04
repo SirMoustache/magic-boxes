5a6d4f50fdb167407f2b0f6a35c57b2448e144db
Vitaliy <sethsleerk@gmail.com>
2018-12-04 17:00:22 +0200

    Add describe to tests

--- src/IO/__tests__/IO.js
+++ src/IO/__tests__/IO.js
@@ -3,20 +3,22 @@ import IO from '../IO';
 const addFoo = x => `${x} foo`;
 const addBar = x => `${x} bar`;
 
-test('IO exist', () => expect(IO).toBeDefined());
+describe('IO  Monad', () => {
+  test('IO exist', () => expect(IO).toBeDefined());
 
-test('IO run() method works', () => expect(IO(x => x).run('run')).toBe('run'));
+  test('run() method works', () => expect(IO(x => x).run('run')).toBe('run'));
 
-test('IO of() method works', () =>
-  expect(IO.of('of course!').run()).toBe('of course!'));
+  test('of() method works', () =>
+    expect(IO.of('of course!').run()).toBe('of course!'));
 
-test('IO from() method works', () =>
-  expect(IO.from(x => x).run('From')).toBe('From'));
+  test('from() method works', () =>
+    expect(IO.from(x => x).run('From')).toBe('From'));
 
-test('IO map() method works from', () =>
-  expect(
-    IO(x => x)
-      .map(addFoo)
-      .map(addBar)
-      .run('map'),
-  ).toBe('map foo bar'));
+  test('map() method works from', () =>
+    expect(
+      IO(x => x)
+        .map(addFoo)
+        .map(addBar)
+        .run('map'),
+    ).toBe('map foo bar'));
+});
